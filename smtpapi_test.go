d58827f941e97ba31a73e4ee8a8fd52fb475b981
elbuo8 <yamil.asusta@sendgrid.com>
2014-01-25 17:46:54 -0600

    added tests to sets

--- smtpapi_test.go
+++ smtpapi_test.go
@@ -7,13 +7,7 @@ import (
 )
 
 func Test_Adds(t *testing.T) {
-	validHeader, _ := json.Marshal([]byte(`{"to":["test@email.com"],
-	"sub":{"subKey":["subValue"]},
-	"section":{"testSection":"sectionValue"},
-	"category":["testCategory"],
-	"unique_args":{"testUnique":"uniqueValue"},
-	"filters":{"testFilter":{"settings":{"filter":"filterValue"}}}}`))
-
+	validHeader, _ := json.Marshal([]byte(`{"to":["test@email.com"],"sub":{"subKey":["subValue"]},"section":{"testSection":"sectionValue"},"category":["testCategory"],"unique_args":{"testUnique":"uniqueValue"},"filters":{"testFilter":{"settings":{"filter":"filterValue"}}}}`))
 	headers := NewSMTPAPIHeader()
 	headers.AddTo("test@email.com")
 	headers.AddSubstitution("subKey", "subValue")
@@ -28,7 +22,34 @@ func Test_Adds(t *testing.T) {
 		if reflect.DeepEqual(testHeader, validHeader) {
 			t.Logf("Success")
 		} else {
-			t.Errorf("Invalid output")
+			t.Errorf("Invalid headers")
+		}
+	}
+}
+
+func Test_Sets(t *testing.T) {
+	validHeader, _ := json.Marshal([]byte(`{"to":["test@email.com"],"sub":{"subKey":["subValue"]},"section":{"testSection":"sectionValue"},"category":["testCategory"],"unique_args":{"testUnique":"uniqueValue"},"filters":{"testFilter":{"settings":{"filter":"filterValue"}}}}`))
+	headers := NewSMTPAPIHeader()
+	headers.SetTos([]string{"test@email.com"})
+	sub := make(map[string][]string)
+	sub["subKey"] = []string{"subValue"}
+	headers.SetSubstitutions(sub)
+	sections := make(map[string]string)
+	sections["testSection"] = "sectionValue"
+	headers.SetSections(sections)
+	headers.SetCategories([]string{"testCategory"})
+	unique := make(map[string]string)
+	unique["testUnique"] = "uniqueValue"
+	headers.SetUniqueArgs(unique)
+	headers.AddFilter("testFilter", "filter", "filterValue")
+	if h, e := headers.JsonString(); e != nil {
+		t.Errorf("Error! %s", e)
+	} else {
+		testHeader, _ := json.Marshal([]byte(h))
+		if reflect.DeepEqual(testHeader, validHeader) {
+			t.Logf("Success")
+		} else {
+			t.Errorf("Invalid headers")
 		}
 	}
 }
