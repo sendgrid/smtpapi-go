d1a8c04976015ce583d2dc705c9ccdd5240a7715
elbuo8 <yamil.asusta@sendgrid.com>
2014-05-02 17:53:58 -0400

    Added comments all exported functions

--- smtpapi.go
+++ smtpapi.go
@@ -14,28 +14,34 @@ type SMTPAPIHeader struct {
 	Filters    map[string]Filter   `json:"filters,omitempty"`
 }
 
+// Filter represents an App/Filter and its settings
 type Filter struct {
 	Settings map[string]string `json:"settings,omitempty"`
 }
 
+// NewSMTPAPIHeader creates a new header struct
 func NewSMTPAPIHeader() *SMTPAPIHeader {
 	return &SMTPAPIHeader{}
 }
 
+// AddTo appends a single email to the To header
 func (h *SMTPAPIHeader) AddTo(email string) {
 	h.To = append(h.To, email)
 }
 
+// AddTos appends multiple emails to the To header
 func (h *SMTPAPIHeader) AddTos(emails []string) {
 	for i := 0; i < len(emails); i++ {
 		h.AddTo(emails[i])
 	}
 }
 
+// SetTos sets the value of the To header
 func (h *SMTPAPIHeader) SetTos(emails []string) {
 	h.To = emails
 }
 
+// AddSubstitution adds a new substitution to a specific key
 func (h *SMTPAPIHeader) AddSubstitution(key, sub string) {
 	if h.Sub == nil {
 		h.Sub = make(map[string][]string)
@@ -43,16 +49,19 @@ func (h *SMTPAPIHeader) AddSubstitution(key, sub string) {
 	h.Sub[key] = append(h.Sub[key], sub)
 }
 
+// AddSubstitutions adds a multiple substitutions to a specific key
 func (h *SMTPAPIHeader) AddSubstitutions(key string, subs []string) {
 	for i := 0; i < len(subs); i++ {
 		h.AddSubstitution(key, subs[i])
 	}
 }
 
+// SetSubstitutions sets the value of the substitutions on the Sub header
 func (h *SMTPAPIHeader) SetSubstitutions(sub map[string][]string) {
 	h.Sub = sub
 }
 
+// AddSection sets the value for a specific section
 func (h *SMTPAPIHeader) AddSection(section, value string) {
 	if h.Section == nil {
 		h.Section = make(map[string]string)
@@ -60,24 +69,29 @@ func (h *SMTPAPIHeader) AddSection(section, value string) {
 	h.Section[section] = value
 }
 
+// SetSections sets the value for the Section header
 func (h *SMTPAPIHeader) SetSections(sections map[string]string) {
 	h.Section = sections
 }
 
+// AddCategory adds a new category to the Category header
 func (h *SMTPAPIHeader) AddCategory(category string) {
 	h.Category = append(h.Category, category)
 }
 
+// AddCategories adds multiple categories to the Category header
 func (h *SMTPAPIHeader) AddCategories(categories []string) {
 	for i := 0; i < len(categories); i++ {
 		h.AddCategory(categories[i])
 	}
 }
 
+// SetCategories will set the value of the Categories field
 func (h *SMTPAPIHeader) SetCategories(categories []string) {
 	h.Category = categories
 }
 
+// AddUniqueArg will set the value of a specific argument
 func (h *SMTPAPIHeader) AddUniqueArg(arg, value string) {
 	if h.UniqueArgs == nil {
 		h.UniqueArgs = make(map[string]string)
@@ -85,10 +99,12 @@ func (h *SMTPAPIHeader) AddUniqueArg(arg, value string) {
 	h.UniqueArgs[arg] = value
 }
 
+// SetUniqueArgs will set the value of the Unique_args header
 func (h *SMTPAPIHeader) SetUniqueArgs(args map[string]string) {
 	h.UniqueArgs = args
 }
 
+// AddFilter will set the specific setting for a filter
 func (h *SMTPAPIHeader) AddFilter(filter, setting, value string) {
 	if h.Filters == nil {
 		h.Filters = make(map[string]Filter)
@@ -101,6 +117,7 @@ func (h *SMTPAPIHeader) AddFilter(filter, setting, value string) {
 	h.Filters[filter].Settings[setting] = value
 }
 
+// SetFilter takes in a Filter struct with predetermined settings and sets it for such Filter key
 func (h *SMTPAPIHeader) SetFilter(filter string, value *Filter) {
 	if h.Filters == nil {
 		h.Filters = make(map[string]Filter)
@@ -108,6 +125,7 @@ func (h *SMTPAPIHeader) SetFilter(filter string, value *Filter) {
 	h.Filters[filter] = *value
 }
 
+// JSONString returns the representation of the Header
 func (h *SMTPAPIHeader) JSONString() (string, error) {
 	headers, e := json.Marshal(h)
 	return string(headers), e
