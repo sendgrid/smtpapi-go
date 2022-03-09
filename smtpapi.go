7a225c5930ae33e2c0cf30a19160d8e8bec1a9f3
Twilio <team_interfaces+github@twilio.com>
2022-03-09 12:21:55 -0800

    Release v0.6.9

--- smtpapi.go
+++ smtpapi.go
@@ -8,7 +8,7 @@ import (
 )
 
 // Version represents the current version of the smtpapi-go library
-const Version = "0.6.8"
+const Version = "0.6.9"
 
 // SMTPAPIHeader will be used to set up X-SMTPAPI params
 type SMTPAPIHeader struct {
